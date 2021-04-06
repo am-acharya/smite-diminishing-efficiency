8675123695d200fc5e934bd71558c118323021c7
am-acharya <arvindmani.satya@gmail.com>
2021-04-06 12:10:19 +0530

    fixes typo, adds ms_calculator

--- movement_speed/movement_speed.py
+++ movement_speed/movement_speed.py
@@ -1,8 +1,18 @@
-def d_movement_speed(undiminshed_movement_speed):
-    if(undiminshed_movement_speed <= 457):
-        return undiminshed_movement_speed
-    if(undiminshed_movement_speed > 457 and undiminshed_movement_speed <= 540.5):
-        return 457 + ((undiminshed_movement_speed - 457) * 0.8)
-    if(undiminshed_movement_speed > 540.5 and undiminshed_movement_speed <= 1000):
-        return 523.8 + ((undiminshed_movement_speed - 540.5) * 0.5)
+def d_movement_speed(undiminished_movement_speed):
+    # undiminshed_movement_speed = sum(movement_speed_list)
+    if(undiminished_movement_speed <= 457):
+        return undiminished_movement_speed
+    if(undiminished_movement_speed > 457 and undiminished_movement_speed <= 540.5):
+        return 457 + ((undiminished_movement_speed - 457) * 0.8)
+    if(undiminished_movement_speed > 540.5 and undiminished_movement_speed <= 1000):
+        return 523.8 + ((undiminished_movement_speed - 540.5) * 0.5)
     return 753.55
+
+def undim_ms (base, percent_ms):
+    return base + ((base*percent_ms)/100)
+
+def ms_calculator (base, item_list):
+    percent_ms = 0
+    for item in item_list:
+        percent_ms += item.ms or 0
+    return d_movement_speed(undim_ms(base, percent_ms))
